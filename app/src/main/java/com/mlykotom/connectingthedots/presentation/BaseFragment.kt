5b6809ddd4ec3159c86b1986ecf5ff44ea8ee9dd
Tomáš Mlynarič <mlynarict@gmail.com>
2021-02-07 21:18:09 +0100

    feat: use dagger.Lazy for factory

--- app/src/main/java/com/mlykotom/connectingthedots/presentation/BaseFragment.kt
+++ app/src/main/java/com/mlykotom/connectingthedots/presentation/BaseFragment.kt
@@ -7,13 +7,16 @@ import com.mlykotom.connectingthedots.di.InjectingSavedStateViewModelFactory
 import javax.inject.Inject
 
 abstract class BaseFragment : Fragment(), HasDefaultViewModelProviderFactory {
+    /**
+     * dagger.Lazy used here, so that injection is request when [getDefaultViewModelProviderFactory] is called
+     */
     @Inject
-    lateinit var defaultViewModelFactory: InjectingSavedStateViewModelFactory
+    lateinit var defaultViewModelFactory: dagger.Lazy<InjectingSavedStateViewModelFactory>
 
     /**
      * This method androidx uses for `by viewModels` method.
      * We can set out injecting factory here and therefore don't touch it again
      */
     override fun getDefaultViewModelProviderFactory(): ViewModelProvider.Factory =
-        defaultViewModelFactory.create(this, arguments)
+        defaultViewModelFactory.get().create(this, arguments)
 }
